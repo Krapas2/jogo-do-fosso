4b4d6c24fb778df962b8fbbef93462084e44d591
Krapas <eduardok04@gmail.com>
2024-07-23 07:42:27 -0300

    gui spawner duplicates fix

--- jogo-do-fosso/Assets/Scripts/Player/Proto/GUI/ProtoGUISpawner.cs
+++ jogo-do-fosso/Assets/Scripts/Player/Proto/GUI/ProtoGUISpawner.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
 
-public class ProtoGUISpawner : MonoBehaviour
+public class ProtoGUISpawner : NetworkBehaviour
 {
     public ProtoGUI protoGUI;
 
     void Start()
     {
+        if(!isLocalPlayer){
+            return;
+        }
+
         Canvas canvas = FindObjectOfType<Canvas>();
 
         ProtoGUI spawnedProtoGUI = Instantiate(protoGUI, canvas.transform);
