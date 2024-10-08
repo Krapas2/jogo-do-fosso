7d2b879d6b0d5f442480a8c617961f00e412e4db
Krapas <eduardok04@gmail.com>
2024-10-08 19:55:05 -0300

    knockback added to proto punch

--- jogo-do-fosso/Assets/Scripts/Player/Characters/Proto/Objects/ProtoPunch.cs
+++ jogo-do-fosso/Assets/Scripts/Player/Characters/Proto/Objects/ProtoPunch.cs
@@ -2,28 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using Mirror.Experimental;
 
 public class ProtoPunch : NetworkBehaviour
 {
     [SyncVar]
     public float damage;
+    [SyncVar]
+    public float knockback;
 
     [HideInInspector]
     public Character owner;
 
+    private CameraData cameraData;
+
+    void Start(){
+        if (isOwned) {
+            cameraData = FindObjectOfType<CameraData>();
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other) 
     {
-        bool otherIsCharacter = other.gameObject.TryGetComponent<Character>(out Character otherCharacter);
-        if(!otherIsCharacter || otherCharacter == owner){
+        if(other.gameObject == owner.gameObject){
             return;
         }
 
-        Damage(otherCharacter);
+
+        if(other.gameObject.TryGetComponent<Character>(out Character otherCharacter)){
+            CmdDamage(otherCharacter);
+        }
+
+        if(other.gameObject.TryGetComponent<NetworkIdentity>(out NetworkIdentity otherIdentity)){
+            Vector2 direction = (cameraData.worldMousePosition - transform.position.Vector2()).normalized;
+            if(otherIdentity.connectionToClient != connectionToClient){
+                TargetKnockback(otherIdentity.connectionToClient, otherIdentity, direction * knockback);
+            }else{
+                OwnedKnockback(otherIdentity, direction * knockback);
+            }
+        }
     }
 
     [Command]
-    void Damage(Character character)
+    void CmdDamage(Character character)
     {
         character.TakeDamage(damage);
+    }
+
+    [TargetRpc]
+    void TargetKnockback(NetworkConnectionToClient target, NetworkIdentity other, Vector2 vector)
+    {
+        Debug.Log(other.gameObject);
+        if(other.gameObject.TryGetComponent<Rigidbody2D>(out Rigidbody2D rigidbody)){
+            rigidbody.velocity = vector;
+        }
+    }
+
+    void OwnedKnockback(NetworkIdentity other, Vector2 vector)
+    {
+        Debug.Log(other.gameObject);
+        if(other.gameObject.TryGetComponent<Rigidbody2D>(out Rigidbody2D rigidbody)){
+            rigidbody.velocity = vector;
+        }
     }
 }
